865d8c09dc4ba4f048ee465a0e3fa933c7d7bf8d
Fla175 <flaviofpomin@gmail.com>
2026-05-22 15:02:05 +0000

    fix(headerSection): correção de hierárquia no headerSection.

--- components/HeaderSection.tsx
+++ components/HeaderSection.tsx
@@ -1,4 +1,5 @@
 // components/HeaderSection.tsx
+import { useUser } from "@/lib/context/UserContext";
 import { Box, PlusCircle } from "lucide-react";
 
 interface HeaderSectionProps {
@@ -6,6 +7,8 @@ interface HeaderSectionProps {
 }
 
 export default function HeaderSection({ onNewActive }: HeaderSectionProps) {
+  const { user } = useUser();
+  const isViewer = user?.role === 'VIEWER';
   return (
     <div className="flex flex-col md:flex-row justify-between items-center gap-4 bg-white dark:bg-zinc-900 p-6 rounded-[2rem] shadow-sm border border-gray-100 dark:border-white/5">
       <div className="flex items-center gap-5 w-full md:w-auto">
@@ -16,13 +19,15 @@ export default function HeaderSection({ onNewActive }: HeaderSectionProps) {
         </div>
       </div>
 
-      <button 
+      { !isViewer &&
+        <button 
         onClick={onNewActive}
         className="w-full md:w-auto bg-blue-600 hover:bg-blue-700 text-white px-8 py-4 rounded-2xl text-xs font-black tracking-wide transition-all flex items-center justify-center gap-3 shadow-lg shadow-blue-500/20 active:scale-95"
       >
         <PlusCircle size={18} />
         Novo Ativo
       </button>
+      }
     </div>
   );
 }
