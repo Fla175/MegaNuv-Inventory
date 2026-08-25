26853f8a05724a0f65f449df9d3a9f5fd7e720fe
Fla175 <flaviofpomin@gmail.com>
2026-08-25 22:07:03 +0000

    chore: Otimização do projeto com saída de build `standalone`.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "standalone",
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
